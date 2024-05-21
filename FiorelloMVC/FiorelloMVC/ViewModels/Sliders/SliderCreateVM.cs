7dee009ded6af0bb2897ceec96e6a8804a5d76ac
rashadaghayev85 <rashadra@code.edu.az>
2024-05-21 04:18:36 +0400

    file upload

---
+++ FiorelloMVC/FiorelloMVC/ViewModels/Sliders/SliderCreateVM.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FiorelloMVC.ViewModels.Sliders
+{
+    public class SliderCreateVM
+    {
+        [Required]
+        public IFormFile Image { get; set; }
+    }
+}
